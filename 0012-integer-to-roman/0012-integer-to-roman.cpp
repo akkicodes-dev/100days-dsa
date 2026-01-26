e06d2d7f3cc99e643271e34bc64047637be520c3
Akash Satao <137327989+akkicodes-dev@users.noreply.github.com>
2026-01-26 09:39:03 +0530

    Time: 2 ms (68.89%), Space: 9.2 MB (84.44%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string result;
-        pair<int, string> values[] = {
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
-        };
+       string romamSymbols[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
-        for (const auto& [value, symbol] : values) {
-            while (num >= value) {
-                result += symbol;
-                num -= value;
+        int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+        string ans ="";
+
+        for(int i=0; i<13; i++){
+            while(num >= values[i]){
+                ans+=romamSymbols[i];//add karte jaynge
+                num -= values[i];//values ko chota karte jaynge means 1000 aya 1994 ke liye to nikal diyaa aur bad me 900 aya to bhi nikal diyaa like that
             }
         }
-
-        return result;
+        return ans;
     }
 };
