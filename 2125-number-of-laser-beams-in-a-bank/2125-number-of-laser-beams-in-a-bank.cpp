7b546fb8fb81076d650eeb2f29d48f4ae12db449
Akash Satao <137327989+akkicodes-dev@users.noreply.github.com>
2026-01-26 10:42:38 +0530

    Time: 8 ms (47.72%), Space: 33.4 MB (6.42%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    int countSetBits(string& binary) {
-        int c = 0;
-        for (auto b : binary)
-            c = b == '1' ? c + 1 : c;
-        return c;
+    int countDevices(string& binary) {
+       int c=0;
+       for(auto b:binary){
+        c += b - '0';//0me se 0 - karunga to 0 hi ayega or 1 me se 1 - minus karunga to  1 aygaaaa
+        
+       }
+       return c;
     }
 
     int numberOfBeams(vector<string>& bank) {
         vector<int> devices;
-        for (auto row : bank)
-            devices.push_back(countSetBits(row));
-        
-        int beams = 0;
-        for (int i = 0; i < devices.size() - 1; ++i) {
-            int j = i + 1;
-            while (j < devices.size()) {
-                beams += devices[i] * devices[j];
-                if (devices[j] == 0) 
-                    ++j;
-                else
+        for (auto row : bank){
+            devices.push_back(countDevices(row));
+        }
+        int beams =0;
+        for(int i=0; i<devices.size()-1;++i){
+            int j=i+1;
+            while(j<devices.size()){
+                beams += devices[i]*devices[j];
+                if(devices[j]==0){
+                    j++;
+                }else{
                     break;
+                }
             }
         }
-        return beams;
+return beams;
+        
     }
 };
