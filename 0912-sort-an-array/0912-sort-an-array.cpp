364f5cfb1151554b4a3044dc42c1432a94b08f5d
Akash Satao <137327989+akkicodes-dev@users.noreply.github.com>
2026-08-18 17:03:56 +0530

    Time: 140 ms (65.06%), Space: 71 MB (76.67%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-      void  merge(vector<int>&v,   vector<int>&temp, int start,int mid,int end){
-        int i = start, j = mid+1, k= start;
-        while(i <= mid && j<= end){
-            if(v[i] <= v[j]) temp[k++] = v[i++];
-            else
-                temp[k++] = v[j++];// 
+      void  merge(vector<int>&v, int start,int mid,int end){
+        int total_len = end - start+1;
+        int gap = total_len/2 + total_len %2;
+        while(gap>0){
+        int i = start, j = start+ gap;
+        while(j <= end){
+            if(v[i] > v[j]){
+                swap(v[i],v[j]);
+            }
+            ++i, ++j;
         }
-        while(i <= mid) temp[k++] = v[i++];
-        while(j <= end) temp[k++] = v[j++];
-        while( start <= end){
-            v[start] = temp[start];
-            start++;
-        }
+        gap = gap <= 1 ? 0 : (gap/2)+ (gap % 2);
       }
-    void  mergeSort(vector<int>&v,   vector<int>&temp, int start, int end){
+    }
+    void  mergeSort(vector<int>&v, int start, int end){
         if(start >= end){
             return;
         }
         int mid =(start + end) >>1;// it means  divde by  two  hogya hai
-        mergeSort(v,temp, start, mid);
-        mergeSort(v,temp, mid+1, end);
-        merge(v,temp, start, mid , end);
+        mergeSort(v, start, mid);
+        mergeSort(v, mid+1, end);
+        merge(v,start, mid , end);
     }
     vector<int> sortArray(vector<int>& nums) {
-        vector<int>temp(nums.size(), 0);
-        mergeSort(nums, temp, 0, nums.size()-1);
+        mergeSort(nums, 0, nums.size()-1);
         return nums;
     }
 };
