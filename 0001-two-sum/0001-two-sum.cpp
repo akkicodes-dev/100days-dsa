0ca34d44f87891d662a2392fc16bb1a053460ff1
Akash Satao <137327989+akkicodes-dev@users.noreply.github.com>
2026-08-27 23:10:40 +0530

    Time: 39 ms (28.94%), Space: 14.2 MB (74.48%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+            int n = nums.size();
+            //lets find out pairs
+            for(int i=0; i<n; i++){
+                for(int j=i+1; j<n;j++){
+                    int first = nums[i];
+                    int second = nums[j];
+
+                    int sum = first + second;
+
+                    if(sum == target){
+                        return {i,j};
+                    }
+                }
+            }
+            return {};
+    }
+};
