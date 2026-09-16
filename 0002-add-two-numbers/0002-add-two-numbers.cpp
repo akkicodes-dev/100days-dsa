101a9946375215fb79abab819c3565df3d5fe44f
Akash Satao <137327989+akkicodes-dev@users.noreply.github.com>
2026-09-16 14:12:30 +0530

    Time: 0 ms (100%), Space: 77.2 MB (12.93%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,28 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+//Iterative Approach
+    // ListNode* iterative(ListNode* l1, ListNode* l2){
+    //      auto ans = new ListNode(-1);
+    //     auto it = ans;
+    //     int c = 0; //Carry
+    //     int sum = 0;
+    //     while(l1 || l2 || c){ //l1 l2 jab tak null nahi  hote ya carry bacha  hua hai tab tak chalta rahega
+
+    //     int a = l1 ? l1->val : 0;
+    //     int b = l2 ? l2->val : 0;
+    //     sum = a+b+c;
+    //     int digit = sum % 10;
+    //     c = sum /10; //carry  update hojayega
+
+    //     // store the digit in  the anwser link list 
+    //      it -> next = new ListNode(digit);
+    //      it = it->next;
+    //      l1 = l1 ? l1 -> next : 0;
+    //      l2 = l2 ? l2 -> next : 0;
+    //     }
+    //     return ans -> next;
+    // }
+
+//Recursive Approach
+     ListNode* recursive(ListNode* l1, ListNode* l2, int  carry = 0) {
+      if(!l1 && !l2 && !carry) return NULL;
+        int a = l1 ? l1->val : 0;
+        int b = l2 ? l2->val : 0;
+        int sum = a+b+carry;
+        int digit = sum % 10;
+        carry = sum /10; //carry  update hojayega
+
+        ListNode* ans = new ListNode(digit);
+        ans -> next = recursive(l1 ? l1->next : l1, l2 ? l2-> next : l2, carry);
+        return ans;
+
+    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* tail = dummyHead;
-        int carry = 0;
-
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int digit1 = (l1 != nullptr) ? l1->val : 0;
-            int digit2 = (l2 != nullptr) ? l2->val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* newNode = new ListNode(digit);
-            tail->next = newNode;
-            tail = tail->next;
-
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
-        }
-
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;
+      // return iterative(l1,l2);
+      
+      return recursive(l1,l2);
     }
 };
